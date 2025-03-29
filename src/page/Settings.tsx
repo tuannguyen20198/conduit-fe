5a5000123d3b2416676c9a0cd0fe414a3c73b41c
matocdo2018 <lynguyenthanhtuan@gmail.com>
2025-03-29 21:54:39 +0700

    done

--- src/page/Settings.tsx
+++ src/page/Settings.tsx
@@ -2,10 +2,6 @@ import SettingsForm from "@/component/SettingForms";
 import { SettingsProps } from "@/interfaces/settings";
 import React from "react";
 
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 
 
 const Settings: React.FC<SettingsProps> = ({ formData, isChanged, handleChange, handleSubmit, error }) => {
